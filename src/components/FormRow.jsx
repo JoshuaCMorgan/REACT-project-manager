8a8b44d5b025a9d4cdbe8ff64f6d6dcf7f655073
Joshua Morgan <jmorg0605@gmail.com>
2024-08-23 10:51:22 -0400

    change form to uncontrolled component using formData
    state management changed from React to formData.
    This helps with keeping re-rendering to none.
    For this form, there is no reason to re-render.

--- src/components/FormRow.jsx
+++ src/components/FormRow.jsx
@@ -1,34 +1,19 @@
 import React from "react";
-export function FormRow({
-  type,
-  name,
-  labelText,
-  textarea,
-  onChange,
-  ...props
-}) {
+export function FormRow({ type, name, labelText, textarea, ...props }) {
   return (
     <div className="form-row">
       <label className="form-label" htmlFor={name}>
         {labelText || name}
       </label>
       {textarea ? (
-        <textarea
-          className="form-input"
-          name={name}
-          id={name}
-          onChange={onChange}
-          required
-        ></textarea>
+        <textarea className="form-input" name={name} id={name}></textarea>
       ) : (
         <input
           className="form-input"
           type={type}
           name={name}
           id={name}
-          onChange={onChange}
           {...props}
-          required
         />
       )}
     </div>
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -3,40 +3,35 @@ import { FormRow } from "./FormRow";
 import { Button } from "./Button";
 import Modal from "./Modal";
 export function NewProject({ onCancel, addNewProject }) {
-  const [formData, setFormData] = useState({
-    title: "",
-    description: "",
-    date: "",
-  });
-
   const modal = useRef();
 
-  function handleChange(event) {
-    const { name, value } = event.target;
-    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
-  }
-
-  function convertDate() {
+  function convertDate(formData) {
     let [year, month, day] = formData.date.split("-");
 
     month = Intl.DateTimeFormat("en", { month: "short" }).format(
       new Date(month)
     );
     formData.date = `${month} ${day}, ${year}`;
+    return formData;
   }
 
-  function formFieldsResult() {
+  function checkEmptyValues(formData) {
     return Object.values(formData).every((value) => value.length > 0);
   }
+
   function handleSubmit(event) {
     event.preventDefault();
-    if (!formFieldsResult()) {
+
+    const formData = new FormData(event.target);
+    const formToObject = Object.fromEntries(formData);
+
+    if (!checkEmptyValues(formToObject)) {
       modal.current.open();
       return false;
     }
 
-    convertDate();
-    addNewProject(formData);
+    const updatedFormToObject = convertDate(formToObject);
+    addNewProject(updatedFormToObject);
   }
 
   return (
@@ -46,25 +41,10 @@ export function NewProject({ onCancel, addNewProject }) {
         <p>Oops ... looks like you forgot to enter a value.</p>
         <p>Please make sure you provide a valid value for every input field.</p>
       </Modal>
-      <form className="form">
-        <FormRow
-          type="text"
-          name="title"
-          labelText="title"
-          onChange={handleChange}
-        />
-        <FormRow
-          name="description"
-          labelText="description"
-          onChange={handleChange}
-          textarea
-        />
-        <FormRow
-          type="date"
-          name="date"
-          labelText="date"
-          onChange={handleChange}
-        />
+      <form onSubmit={handleSubmit} className="form">
+        <FormRow type="text" name="title" labelText="title" />
+        <FormRow name="description" labelText="description" textarea />
+        <FormRow type="date" name="date" labelText="date" />
         <menu className="buttonGroup">
           <li>
             <Button className="btn  btnClear" onClick={onCancel}>
@@ -72,9 +52,7 @@ export function NewProject({ onCancel, addNewProject }) {
             </Button>
           </li>
           <li>
-            <Button type="button" onClick={handleSubmit}>
-              save
-            </Button>
+            <Button type="submit">save</Button>
           </li>
         </menu>
       </form>
